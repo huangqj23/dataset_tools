ad2c2ce2bfc6f985c69b19963309301151d7fb1b
朦胧月 <13260590401@163.com>
2024-12-06 13:27:43 +0800

    update video to image func

--- examples/image_video_example.py
+++ examples/image_video_example.py
@@ -5,12 +5,23 @@ if __name__ == '__main__':
     converter = VideoImageConverter()
     
     # 图片转视频示例
-    converter.images_to_video(
-        image_dir='/data1/huangqj/images/',
-        output_path='output.mp4',
-        fps=30,
-        image_format='.JPG',
-        frames_per_image=30,
-        num_workers=8
-    )
+    # converter.images_to_video(
+    #     image_dir='/data1/huangqj/images/',
+    #     output_path='output.mp4',
+    #     fps=30,
+    #     resize=(1920, 1080),
+    #     image_format='.JPG',
+    #     frames_per_image=30,
+    #     transition_frames=15,
+    #     num_workers=8
+    # )
     
+    # 视频转图片
+    converter.video_to_images(
+        video_path='output.mp4',
+        output_dir='output_frames',
+        frame_interval=30,  # 每5帧提取一帧
+        image_format='.jpg',
+        image_quality=95,
+        resize=(1920, 1080)  # 可选的尺寸调整
+    )
